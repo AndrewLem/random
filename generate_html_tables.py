e7d0e238cd98044de17835ba42d4a3f4f3cad99d
Andrew Lem <Andrew.Lem@ga.gov.au>
2019-02-12 11:49:52 +1100

    added statement shuffle excluded individual from their sheet added context and greeting statement added background image

--- generate_html_tables.py
+++ generate_html_tables.py
@@ -1,4 +1,5 @@
 import os
+from random import shuffle
 
 base_folder = 'test_statements'
 files = os.listdir(base_folder)
@@ -6,13 +7,40 @@ grads_statements = []
 rows = 4
 columns = 4
 tables_folder = 'html_tables'
-html_header = " <html> " \
-              "<head> " \
-              "<style> " \
-              "table, th, td { border: 1px solid black; border-collapse: collapse; } " \
-              "</style> " \
-              "</head>"
-html_footer = '</html>'
+html_header = """ 
+<html> 
+    <head> 
+        <style> 
+            table{ 
+                border: 1px solid black; 
+                border-collapse: collapse;
+                width: 100%;
+            } 
+            td{
+                border: 1px solid black;
+                padding: 5px;
+                padding-bottom: 180px;
+                width: 25%;
+                text-align: left;
+            }
+            body{
+                background-image: url("background_stone.jpg");
+                background-repeat: no-repeat;
+                background-size: auto;
+            }
+        </style> 
+    </head>
+"""
+intro = """
+    <body>
+        <h1>2019 Graduate Canberra Tour People Bingo!</h1>
+        Hi %s!<p>
+        Can you fill in your bingo sheet? Match true statements to people's names!<p> 
+        You'll notice there's 16 squares and only 13 other people, 
+        so 3 people will appear on your sheet twice!<p>
+        Share your favourite true statements/stories at the end of the day!<p>
+        """
+html_footer = '</body></html>'
 table_header = '<table>'
 table_footer = '</table>'
 row_start = '<tr>'
@@ -27,18 +55,35 @@ for file in files:
 
 d1 = -1
 d2 = -1
+output_lists = []
 for i in range(0, len(grads_statements)):
-    table_string = html_header + table_header
+    output_list = []
     for j in range(0, rows):
-        table_string += row_start
         for k in range(0, columns):
             d1 = (d1 + 1) % len(grads_statements)
             if d1 == 0:
                 d2 = d2 + 1
-            table_string += cell_start
-            table_string += grads_statements[d1][d2 % len(grads_statements[d1])]
-            table_string += cell_end
-        table_string += row_end
+            if d1 == i:
+                d1 = (d1 + 1) % len(grads_statements)
+            output_list.append(grads_statements[d1][d2 % len(grads_statements[d1])])
+    output_lists.append(output_list)
+
+
+i = 0
+j = 0
+for output_list in output_lists:
+    shuffle(output_list)
+    table_string = html_header + intro % files[j].replace('.txt', '') + table_header
+    for output in output_list:
+        if i % columns == 0:
+            table_string += row_start
+        i += 1
+        table_string += cell_start
+        table_string += output
+        table_string += cell_end
+        if i % columns == 0:
+            table_string += row_end
     table_string += table_footer + html_footer
-    with open(os.path.join(tables_folder, f"{i}.html"), 'w') as f:
+    j += 1
+    with open(os.path.join(tables_folder, f"{j}.html"), 'w') as f:
         f.write(table_string)
